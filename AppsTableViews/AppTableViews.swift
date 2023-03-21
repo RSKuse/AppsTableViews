c1cc025a437f2849ae09516afcb6b133e9539bda
Gugulethu <mhlangagc@users.noreply.github.com>
2023-03-21 19:03:45 +0200

    Updated the TableViewCell constraints

--- AppsTableViews/AppTableViews.swift
+++ AppsTableViews/AppTableViews.swift
@@ -29,60 +29,55 @@ class AppTableViews: UITableViewCell {
     }()
     
     lazy var googleLogoImage: UIImageView = {
-     let imageView = UIImageView()
-     imageView.image = UIImage(named: "google_maps_icon")
-     imageView.contentMode = .scaleAspectFill
-     imageView.clipsToBounds = true
-     imageView.translatesAutoresizingMaskIntoConstraints = false
-     return imageView
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "google_maps_icon")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
     }()
     
-    var theSwitch: UISwitch {
+    lazy var theSwitch: UISwitch = {
         let theSwitch = UISwitch()
         theSwitch.tintColor = .green
         theSwitch.isOn = true
-        theSwitch.translatesAutoresizingMaskIntoConstraints = true
+        theSwitch.translatesAutoresizingMaskIntoConstraints = false
         return theSwitch
-    }
+    }()
     
     lazy var titleDetailLabelStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [titleLabel, detailLabel])
         stackView.axis = .vertical
         stackView.distribution = .fillProportionally
-        stackView.spacing = 10.0
+        stackView.spacing = 6.0
         stackView.alignment = .leading
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
 
-
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: UITableViewCell.CellStyle.default, reuseIdentifier: reuseIdentifier)
         
-        addSubview(titleDetailLabelStackView)
         addSubview(googleLogoImage)
         addSubview(theSwitch)
-      
-        titleDetailLabelStackView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        titleDetailLabelStackView.leftAnchor.constraint(equalTo: leftAnchor, constant: 50).isActive = true
-        titleDetailLabelStackView.rightAnchor.constraint(equalTo: rightAnchor, constant: -50).isActive = true
-       
+        addSubview(titleDetailLabelStackView)
         
         googleLogoImage.heightAnchor.constraint(equalToConstant: 40).isActive = true
         googleLogoImage.widthAnchor.constraint(equalToConstant: 40).isActive = true
-        googleLogoImage.leftAnchor.constraint(equalTo:titleDetailLabelStackView.leftAnchor, constant: -45).isActive = true
-        googleLogoImage.topAnchor.constraint(equalTo: titleDetailLabelStackView.topAnchor, constant: 0).isActive = true
-        
-//        theSwitch.heightAnchor.constraint(equalToConstant: 40).isActive = true
-//        theSwitch.widthAnchor.constraint(equalToConstant: 40).isActive = true
-//        theSwitch.leftAnchor.constraint(equalTo:titleDetailLabelStackView.rightAnchor, constant: -45).isActive = true
-//        theSwitch.topAnchor.constraint(equalTo: titleDetailLabelStackView.topAnchor, constant: 0).isActive = true
-        
-        
+        googleLogoImage.leftAnchor.constraint(equalTo: leftAnchor, constant: 20).isActive = true
+        googleLogoImage.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 0).isActive = true
 
+    
+        theSwitch.rightAnchor.constraint(equalTo: rightAnchor, constant: -20).isActive = true
+        theSwitch.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 0).isActive = true
+        
+        titleDetailLabelStackView.leftAnchor.constraint(equalTo: googleLogoImage.rightAnchor,
+                                                        constant: 16).isActive = true
+        titleDetailLabelStackView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        titleDetailLabelStackView.rightAnchor.constraint(equalTo: theSwitch.leftAnchor,
+                                                         constant: -16).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
-    
     
 }
